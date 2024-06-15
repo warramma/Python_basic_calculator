b50f85ed809df7f715ef50631902c96bd511a538
warramma <wishmorethewitch@gmail.com>
2024-06-15 15:52:55 -0500

    start continuous calc

---
+++ calculator_continuous.py
@@ -0,0 +1,19 @@
+sentinel = input('press y to turn calculator on...: ')
+if(sentinel.lower()=='y'):
+    print('powering on....')
+else:
+    print('....okay....bye ig....')
+
+first_num = 0
+result = 0
+second_num = 0
+
+# enter a number to get started
+# that first number get assigned to result, from here we just update result
+# choose an operation
+# enter a second number
+#apply operation, display result
+#if chance to power off
+#to repeat ->
+#choose operator
+#enter number
